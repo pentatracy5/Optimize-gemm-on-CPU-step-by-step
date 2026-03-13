415ec1db108db232770a648376809df325b2b640
79391 <79391@LUPUER>
2026-03-13 13:23:07 +0800

    初始化矩阵并增强参数校验与内存管理
    新增 InitABC 函数用于初始化矩阵 A、B、C，A 和 B 赋值为 1~4 循环，C 初始化为 0。main 函数增加了对 M、N、K 参数有效性的检查，动态分配并释放了矩阵内存，提升了程序健壮性和初始化流程。尚未实现矩阵乘法核心逻辑。

--- BLIS.cpp
+++ BLIS.cpp
@@ -1,14 +1,25 @@
 ﻿#include <iostream>
 #include <sstream>
+#include <algorithm>
 
 using std::cout;
 using std::endl;
 using std::istringstream;
 using std::cerr;
+using std::fill;
 
 #define A(i, k) A[(i) * K + (k)] // M * K
 #define B(k, j) B[(k) * N + (j)] // K * N
 #define C(i, j) C[(i) * N + (j)] // M * N
+
+void InitABC(int M, int N, int K, float* A, float* B, float* C)
+{
+	for (size_t i = 0; i < M * K; i++)
+		A[i] = (float)(i % 4 + 1);
+	for (size_t i = 0; i < K * N; i++)
+		B[i] = (float)(i % 4 + 1);
+	fill(C, C + M * N, 0);
+}
 
 int main(int argc, char* argv[])
 {
@@ -24,4 +35,19 @@ int main(int argc, char* argv[])
 		cerr << "Error: invalid integer arguments." << endl;
 		return 1;
 	}
+	if (M <= 0 || N <= 0 || K <= 0)
+	{
+		cerr << "Error: invalid arguments integer value." << endl;
+		return 1;
+	}
+
+	float* A = (float*)malloc(sizeof(float) * M * K);
+	float* B = (float*)malloc(sizeof(float) * K * N);
+	float* C = (float*)malloc(sizeof(float) * M * N);
+
+	InitABC(M, N, K, A, B, C);
+
+	free(A);
+	free(B);
+	free(C);
 }
