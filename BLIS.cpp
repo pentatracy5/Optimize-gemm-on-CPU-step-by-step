25e6890e5703b970c70d673e6e3d854db288b1b3
79391 <79391@LUPUER>
2026-03-13 12:22:22 +0800

    添加项目文件。

---
+++ BLIS.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#define A(i, k) A[(i) * K + (k)] // M * K
+#define B(k, j) B[(k) * N + (j)] // K * N
+#define C(i, j) C[(i) * N + (j)] // M * N
+
+int main(int argc, char* argv[])
+{
+	int M;
+	int N;
+	int K;
+	if (argc != 4)
+	{
+		cout << "Error: require 3 arguments, but " << argc - 1 << " provided." << endl;
+		exit(0);
+	}
+}
